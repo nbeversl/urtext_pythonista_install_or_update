4067418e214289ed3f57bb7024d5841408f92460
Nate Beversluis <nathanielbeversluis@gmail.com>
2023-06-05 13:22:40 -0400

    remove custom files and launch scripts

--- urtext_pythonista/launch_sublemon_editor.py
+++
@@ -1,7 +0,0 @@
-"""
-Start Editor
-"""
-from sublemon.editor import BaseEditor
-s = BaseEditor({}) # no arguments
-s.setup_buttons(None) # default
-s.show()
--- urtext_pythonista/urtext_theme_light_custom.py
+++
@@ -1,9 +0,0 @@
-from urtext_theme_light import urtext_theme_light
-from objc_util import ObjCClass
-
-urtext_theme_light_custom = urtext_theme_light
-
-urtext_theme_light_custom['font'] = {
-    'regular' :  ObjCClass('UIFont').fontWithName_size_('Fira Code', 12),
-    'bold' :    ObjCClass('UIFont').fontWithName_size_('FiraCode-Bold', 12),
-}
