2ae770a154b02f4bdd1eca6017e40540682403fc
Nate Beversluis <nathanielbeversluis@gmail.com>
2023-09-14 14:09:01 -0400

    restore sublemon themes folder

---
+++ sublemon/themes/colors.py
@@ -0,0 +1,20 @@
+from objc_util import UIColor
+
+#examples
+
+colors = {
+    'paper' :           UIColor.colorWithRed(0.898, green=0.8667, blue=0.8627, alpha=1.0),
+    'grey5' :           UIColor.colorWithRed(0.3294, green=0.3294, blue=0.3294, alpha=1.0),
+    'grey6' :           UIColor.colorWithRed(0.4, green=0.4, blue=0.4, alpha=1.0),
+    'unobtrusive' :     UIColor.colorWithRed(0.8392, green=0.8118, blue=0.7961, alpha=1.0),
+    'white' :           UIColor.whiteColor(),
+    'red' :             UIColor.colorWithRed(1.000, green=0.286, blue=0.424, alpha=1.0),
+    'blue_lighter' :    UIColor.colorWithRed(0.5098, green=0.6, blue=0.8471, alpha=1.0),
+    'blue_brighter' :   UIColor.colorWithRed(0.28, green=0.44, blue=0.92, alpha=1.0),
+    'deep_blue2' :      UIColor.colorWithRed(0.7647, green=0.5098, blue=0.8471, alpha=1.0),
+    'lime2' :           UIColor.colorWithRed(0.2745, green=0.4588, blue=0.4078, alpha=1.0),
+    'bright_green2' :   UIColor.colorWithRed(0.451, green=0.698, blue=0.4196, alpha=1.0),
+    'aqua_green2' :     UIColor.colorWithRed(0.6157, green=0.7686, blue=0.7176, alpha=1.0),
+    'grey_reg' :        UIColor.grayColor(),
+    'lightgray' :       UIColor.lightGrayColor(),
+}
